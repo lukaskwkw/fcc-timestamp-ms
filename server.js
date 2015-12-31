0ecfaa7f015b49209ff71217dc19dad509aa9c50
lukaskwkw <lukaskwkw@gmail.com>
2015-12-31 13:33:56 +0100

    fix

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var parseDate = require("./dateParser.js");
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(process.cwd()))
 app.get("/:date",function  (req,res) {
 	res.send(JSON.stringify(parseDate(req.params.date)));
 })
 
-app.listen(5000);
-console.log("Server running...");
+app.listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
